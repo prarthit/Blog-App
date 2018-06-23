e346e49ac96fbfc9196e6d72b8cf7e4f651401d3
Prarthit Mehra <32339911+prarthit@users.noreply.github.com>
2018-06-23 22:52:01 +0530

    Blog app with add new posts created

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/blog_app");
+
+// APP CONFIG
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog", blogSchema)
+
+// RESTFUL ROUTES
+
+app.get("/", function(req,res){
+	res.redirect("/blogs");
+});
+
+// INDEX ROUTE
+app.get("/blogs", function(req,res){
+	Blog.find({},function(err,blogs){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("index.ejs", {blogs: blogs});
+		}
+	})
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req,res){
+	res.render("new.ejs");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req,res){
+	Blog.create(req.body.blog, function(err,newBlog){
+		if(err){
+			res.render("new.ejs")
+		}
+		else{
+			res.redirect("/blogs");
+		}
+	})
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.render("show.ejs", {blog: foundBlog});
+		}
+	})
+});
+
+app.listen(3000,function(){
+	console.log("Blog app server running!");
+});
